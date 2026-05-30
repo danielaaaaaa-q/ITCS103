bfcbe45bcb2a7b858851c24eef6faff49b57019a
dynneal <dynn5723@gmail.com>
2026-05-30 19:55:47 +0800

    Add files via upload

--- Antonio_FinalProject.py
+++ Antonio_FinalProject.py
@@ -2,6 +2,12 @@ import tkinter as tk
 from tkinter import ttk, messagebox
 import openpyxl as op
 
+def clear_entries():
+    cname_entry.delete(0, tk.END)
+    item_prod.set("")
+    qty_entry.delete(0, tk.END)
+    price_entry.delete(0, tk.END)
+
 def input_validation():
     name = cname_entry.get()
     product = item_prod.get()
@@ -41,7 +47,10 @@ def append_excel():
     wbk = op.load_workbook("Antonio_Database.xlsx")
     sheet = wbk.active
 
-    new_order_id = sheet.max_row
+    if sheet.max_row == 1:
+        new_order_id = 1
+    else:
+        new_order_id = sheet.cell(row=sheet.max_row, column=1).value + 1
 
     sheet.append([new_order_id, name, product, qty, price, total_amount])
 
@@ -49,6 +58,7 @@ def append_excel():
 
     wbk.save("Antonio_Database.xlsx")
     display_order()
+    clear_entries()
 
 def auto_populate(event):
     selected = table.focus()
@@ -64,7 +74,6 @@ def auto_populate(event):
         item_prod.insert(0, values[2])
         qty_entry.insert(0, values[3])
         price_entry.insert(0, values[4])
-
 
 def update():
     selected = table.focus()
@@ -99,6 +108,8 @@ def update():
 
     messagebox.showinfo("Success", "Order updated successfully!")
 
+    clear_entries()
+
 def delete():
     selected = table.focus()
 
@@ -124,7 +135,10 @@ def delete():
     wbk.save("Antonio_Database.xlsx")
 
     messagebox.showinfo("Success", "Record deleted successfully!")
+
     display_order()
+
+    clear_entries()
 
 def auto_price(event):
     if item_prod.get() == "Spanish Latte":
@@ -157,6 +171,21 @@ def auto_price(event):
     elif item_prod.get() == "Fries":
         price_entry.delete(0, tk.END)
         price_entry.insert(0, "100")
+    elif item_prod.get() == "Chicken Alfredo":
+        price_entry.delete(0, tk.END)
+        price_entry.insert(0, "220")
+    elif item_prod.get() == "Mushroom Soup":
+        price_entry.delete(0, tk.END)
+        price_entry.insert(0, "150")
+    elif item_prod.get() == "Grilled Cheese Sandwich":
+        price_entry.delete(0, tk.END)
+        price_entry.insert(0, "120")
+    elif item_prod.get() == "Iced Americano":
+        price_entry.delete(0, tk.END)
+        price_entry.insert(0, "100")
+    elif item_prod.get() == "Chocolate Cake":
+        price_entry.delete(0, tk.END)
+        price_entry.insert(0, "150")
     
     return
 
@@ -167,63 +196,68 @@ window.geometry("1225x500")
 window.configure(bg="pink")
 
 # Form Title
-title = tk.Label(window, text="Velvet Beans Cafe", font=("Times New Roman", 20, "bold"), bg="pink")
-title.grid(row=0, column=0, columnspan=6)
+title = tk.Label(window, text="Velvet Beans Cafe", font=("Century Gothic", 25, "bold"), bg="pink")
+title.grid(row=0, column=0, columnspan=6, pady=10)
 
 # Frame
-genframe = tk.Frame(window, bg="lightblue", bd=2, relief="groove")
+genframe = tk.Frame(window, bg="pink", bd=3, relief="groove")
 genframe.grid(row=1, column=0, columnspan=7, padx=10, pady=10)
 
 # Customer Name Entry
-cname_entry = tk.Entry(genframe, font=("Poppins", 12))
+cname_entry = tk.Entry(genframe, font=("Poppins", 15))
 cname_entry.grid(row=2, column=1, columnspan=2, padx=10, pady=(10, 0))
 
-cname_label = tk.Label(genframe, text="Customer Name", font=("Poppins", 10, "italic"), bg="lightblue")
+cname_label = tk.Label(genframe, text="Customer Name", font=("Poppins", 13, "italic"), bg="pink")
 cname_label.grid(row=3, column=1, columnspan=2)
 
-product_label = tk.Label(genframe, text="Product", font=("Poppins", 10, "italic"), bg="lightblue")
-
+# Product Combobox
+product_label = tk.Label(genframe, text="Product", font=("Poppins", 13, "italic"), bg="pink")
 product_label.grid(row=3, column=3, columnspan=2)
-#product combobox
-item_prod = ttk.Combobox(genframe, font=("Poppins", 12))
+
+item_prod = ttk.Combobox(genframe, font=("Poppins", 15))
 
 item_prod['values'] = (
                         "Spanish Latte", 
-                       "Matcha Latte", 
+                       "Matcha Latte",
+                       "Caramel Macchiato",
                        "Carbonara", 
                        "French Toast", 
-                       "Caramel Macchiato", 
+                       "Chocolate Cake",
                        "Tiramisu", 
                        "Strawberry Cheesecake", 
                        "Blueberry Cheesecake", 
                        "Caesar Salad",
-                       "Fries"
+                       "Fries",
+                       "Chicken Alfredo",
+                       "Mushroom Soup",
+                       "Grilled Cheese Sandwich",
+                       "Iced Americano"
                        )
 
 item_prod.grid(row=2, column=3, columnspan=2, padx=10, pady=(10, 0))
 
 # Quantity Entry
-qty_entry = tk.Entry(genframe, font=("Poppins", 12))
+qty_entry = tk.Entry(genframe, font=("Poppins", 15))
 qty_entry.grid(row=4, column=1, columnspan=2, padx=10, pady=(10, 0))
 
-qty_label = tk.Label(genframe, text="Quantity", font=("Poppins", 10, "italic"), bg="lightblue")
+qty_label = tk.Label(genframe, text="Quantity", font=("Poppins", 13, "italic"), bg="pink")
 qty_label.grid(row=5, column=1, columnspan=2)
 
 # Price Entry
-price_entry = tk.Entry(genframe, font=("Poppins", 12))
+price_entry = tk.Entry(genframe, font=("Poppins", 15))
 price_entry.grid(row=4, column=3, columnspan=2, padx=10, pady=(10, 0))
 
-price_label = tk.Label(genframe, text="Price", font=("Poppins", 10, "italic"), bg="lightblue")
+price_label = tk.Label(genframe, text="Price", font=("Poppins", 13, "italic"), bg="pink")
 price_label.grid(row=5, column=3, columnspan=2)
 
 # Buttons
-submit_btn = tk.Button(window, text="Submit", font=("Poppins", 12, "bold"), bg="yellow", command=append_excel)
+submit_btn = tk.Button(window, text="Submit", font=("Poppins", 13, "bold"), bg="lightblue", command=append_excel)
 submit_btn.grid(row=6, column=1, padx=(90, 0), pady=(10, 20))
 
-update_btn = tk.Button(window, text="Update",font=("Poppins", 12, "bold"), bg="lightgreen", command=update)
+update_btn = tk.Button(window, text="Update",font=("Poppins", 13, "bold"), bg="lightgreen", command=update)
 update_btn.grid(row=6, column=2, padx=(140, 0), pady=(10, 20))
 
-delete_btn = tk.Button(window, text="Delete", bg="red", fg="white",font=("Poppins", 12, "bold"), command=delete)
+delete_btn = tk.Button(window, text="Delete", bg="red", fg="white",font=("Poppins", 13, "bold"), command=delete)
 delete_btn.grid(row=6, column=3, padx=(140, 0), pady=(10, 20))
 
 # Table
